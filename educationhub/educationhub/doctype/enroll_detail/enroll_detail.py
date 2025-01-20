5f86e7e150cc2a4a39f487da9d96e4e39c8f230e
KhushiIdesign <khushi@idesignmylife.net>
2025-01-20 02:16:00 +0000

    changes in enroll-detail

--- educationhub/educationhub/doctype/enroll_detail/enroll_detail.py
+++ educationhub/educationhub/doctype/enroll_detail/enroll_detail.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, EducationHub and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EnrollDetail(Document):
-	pass
+	def before_save(self):
+		seats = frappe.db.get_value('Course',self.course_name,'no_of_seats')
+		if seats is not None: 
+			frappe.db.set_value('Course',self.course_name,'no_of_seats',(int(seats)-1))
+		else:
+			frappe.throw(f"Seats are full for the course {self.course_name} and the no of Seats are {self.no_of_seats}")
+	
--- educationhub/educationhub/doctype/student/student.py
+++ educationhub/educationhub/doctype/student/student.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, EducationHub and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Student(Document):
 	pass
+	
+	
